2b1a5e67e20adedfb7e155221125b6fc0fe6ec04
ialej001 <ivan.alejandre@gmail.com>
2018-11-01 19:11:12 -0700

    Update p4.py

--- p4.py
+++ p4.py
@@ -1,27 +1,31 @@
 def simplePic():
   pic = makePicture(pickAFile())
-    
+  
+  file = r"C:\\Temp\shrunk.jpg"
+  
   newPic = shrink(pic)
 
   show(newPic)
   
+  writePictureTo(newPic, file)
+  
+  
 # shrinks image by half
 def shrink(pic):
-  picWidth = getWidth(pic)
-  picHeight = getHeight(pic)
+  picWidth = getWidth(pic) / 2
+  picHeight = getHeight(pic) / 2
   x2 = 0
   y2 = 0
   
-  shrunkPic = makeEmptyPicture(picWidth/2, picHeight/2)
+  shrunkPic = makeEmptyPicture(picWidth, picHeight)
   
-  for x in range (0, picWidth, 2):
-    for y in range (0, picHeight, 2):
-      currentPixel = getPixel(pic, x, y)
-      newPixel = getPixel(shrunkPic, x2, y2)
+  print(picHeight)
+  print(picWidth)
+  for x in range (0, picWidth):
+    for y in range (0, picHeight):
+      currentPixel = getPixel(pic, (x * 2), (y * 2))
+      newPixel = getPixel(shrunkPic, x, y)
       pixelColor = getColor(currentPixel)
       setColor(newPixel, pixelColor)
-      y2 = y2 + 1
-    x2 = x2 + 1
-
   
   return shrunkPic
